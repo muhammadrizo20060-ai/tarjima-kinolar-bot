5992d312b7e0d9dbb4f0bd89c1b93f8630e3baa9
muhammadrizo20060-ai <muhammadrizo20060@gmail.com>
2026-04-24 21:43:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, fil
 
 # Kinolar bazasi - Siz yuborgan yangi ID bilan
 movies = {
-    "100": "BAACAgEAAxkBAAMVaetLZu51DnkuL-rCwMXToK2zrG4AAv4LAAIyYZlGUwRpc0DEBFc7BA",
-    "3": "BAACAgIAAxkBAAM9aet2bV9oT5unRHk3KDohDAABl-xhAAIGgwACLfChSzI6-b2gd2p8OwQ",
-    "1": "BAACAgQAAxkBAANAaet2sY5LSEy-9DlGTIC5o51JSyUAAqoQAAL64MlTtWhK3fzIdRU7BA",
-    "2": "BAACAgQAAxkBAANCaet2t8_T4fj4YZhh-IWbQQ9Pqh8AAnEYAALNDqFQSXXFhcqD2ks7BA",
-    "101": "BAACAgIAAxkBAAM5aety54oze2DLgHHubjCt1ZyNlrEAAtacaALj2GFJQfQUnnOMvCo7BA"
+    "1": ["Mening aybim (2023)", "BAACAgQAAxkBAANAaet2sY5LSEy-9DlGTIC5o51JSyUAAqoQAAL64MlTtWhK3fzIdRU7BA"],
+    "2": ["Sening aybing (2024)", "BAACAgQAAxkBAANCaet2t8_T4fj4YZhh-IWbQQ9Pqh8AAnEYAALNDqFQSXXFhcqD2ks7BA"],
+    "3": ["Bizning aybimiz (2025)", "BAACAgIAAxkBAAM9aet2bV9oT5unRHk3KDohDAABl-xhAAIGgwACLfChSzI6-b2gd2p8OwQ"],
+    "100": ["O'zaro qizlar orasida", "BAACAgEAAxkBAAMVaetLZu51DnkuL-rCwMXToK2zrG4AAv4LAAIyYZlGUwRpc0DEBFc7BA"],
+    "101": ["Kickboxer", "BAACAgIAAxkBAAM5aety54oze2DLgHHubjCt1ZyNlrEAAtacaALj2GFJQfQUnnOMvCo7BA"],
 }
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Assalomu alaykum! Kino kodini yuboring.")
